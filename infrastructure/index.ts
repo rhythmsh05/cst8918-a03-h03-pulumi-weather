aac401befc2bed682a835e775601131c0061f435
John Vinh <john.michael.vinh2@gmail.com>
2025-01-23 18:41:55 -0500

    Remove exports

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -41,6 +41,3 @@ const registryCredentials = containerregistry
       password: creds.passwords![0].value!,
     }
   })
-
-export const acrServer = registry.loginServer
-export const acrUsername = registryCredentials.username
